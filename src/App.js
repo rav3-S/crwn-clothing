592aa66923088733b39319835fccb8de4f4103b7
Ravi <ravishankar.ma34@gmail.com>
2024-01-26 20:49:48 +0530

    basic template

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
+import "./categories.styles.scss";
+
 const App = () => {
-  return <div>Hello World</div>;
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+    },
+    {
+      id: 4,
+      title: "Womens",
+    },
+    {
+      id: 5,
+      title: "Mens",
+    },
+  ];
+  return (
+    <div classname="categories-container">
+      {categories.map(({ title, id }) => (
+        <div key={id} classname="category-container">
+          <div classname="background-image" />
+          <div classname="category-body">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default App;
