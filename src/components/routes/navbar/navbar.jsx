c46b574c55b367e3d78c0914d8fa84bdb1431fbe
Ravi <ravishankar.ma34@gmail.com>
2024-02-18 15:12:05 +0530

    Added basic Routing with react-router-dom

---
+++ src/components/routes/navbar/navbar.jsx
@@ -0,0 +1,25 @@
+import { Outlet, Link } from "react-router-dom";
+import { ReactComponent as CrwnLogo } from "../../../assets/crown.svg";
+import "./navbar.styles.scss";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="navbar">
+        <Link className="logo-container" to="/">
+          <div className="logo">
+            <CrwnLogo />
+          </div>
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  );
+};
+
+export default Navbar;
